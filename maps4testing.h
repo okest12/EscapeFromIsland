fbc7bb8b7450262d2345c4f6f83e86bcef5bdc49
okest12 <49010388+okest12@users.noreply.github.com>
2019-04-19 11:00:38 +0800

    Add files via upload
    maps for testing purpose

---
+++ maps4testing.h
@@ -0,0 +1,23 @@
+    const char JurassicIslandMap[NUMBER_ROW][NUMBER_COLUMN] = {
+        {'B','W','W','B','B','W','B','W','W'},
+        {'B','L','L','V','L','L','L','L','B'},
+        {'W','L','L','L','L','D','L','L','B'},
+        {'B','L','L','D','L','L','L','L','W'},
+        {'B','L','D','L','L','L','L','L','W'},
+        {'W','L','L','L','L','L','V','L','B'},
+        {'W','V','L','L','L','L','L','L','W'},
+        {'W','L','L','L','D','L','L','L','W'},
+        {'B','B','W','B','W','B','B','W','B'}};
+    const char JurassicIslandMap[NUMBER_ROW][NUMBER_COLUMN] = {
+        {'B','W','W','B','B','W','B','W','W'},
+        {'W','W','L','V','L','L','L','L','B'},
+        {'W','L','L','L','L','D','L','L','B'},
+        {'B','L','L','D','L','L','L','L','W'},
+        {'B','L','D','L','L','L','L','L','W'},
+        {'W','L','L','L','L','L','V','L','B'},
+        {'W','V','L','L','L','L','L','L','W'},
+        {'W','L','L','L','D','L','L','L','W'},
+        {'B','B','W','B','W','B','B','W','B'}};
+    const char JurassicIslandMap[NUMBER_ROW][NUMBER_COLUMN] = {
+        {'B','B'},
+        {'B','B'}};
